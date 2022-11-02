88e89a33cd153d5c45f1b2d9479b3a798201141b
muhamad luthfi sadli <luthfisad1806@gmail.com>
2022-11-02 15:27:44 +0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,3 @@
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js1', '../particlesjs-config.json');
+particlesJS.load('particles-js2', '../particlesjs-config.json');
---
+++ js/basic.js
@@ -0,0 +1,100 @@
+// Set the width and height for the viewport
+
+function resizeViewport() {
+
+	var width = $(window).width(),
+		height = $(window).height(),
+		options = $('.flipbook').turn('options');
+
+	$('.flipbook').removeClass('animated');
+
+	$('.flipbook-viewport').css({
+		width: width,
+		height: height
+	}).
+	zoom('resize');
+
+
+	if ($('.flipbook').turn('zoom')==1) {
+		var bound = calculateBound({
+			width: options.width,
+			height: options.height,
+			boundWidth: Math.min(options.width, width),
+			boundHeight: Math.min(options.height, height)
+		});
+
+		if (bound.width%2!==0)
+			bound.width-=1;
+
+			
+		if (bound.width!=$('.flipbook').width() || bound.height!=$('.flipbook').height()) {
+
+			$('.flipbook').turn('size', bound.width, bound.height);
+
+			if ($('.flipbook').turn('page')==1)
+				$('.flipbook').turn('peel', 'br');
+
+			$('.next-button').css({height: bound.height, backgroundPosition: '-38px '+(bound.height/2-32/2)+'px'});
+			$('.previous-button').css({height: bound.height, backgroundPosition: '-4px '+(bound.height/2-32/2)+'px'});
+		}
+
+		$('.flipbook').css({top: -bound.height/2, left: -bound.width/2});
+	}
+
+	var flipbookOffset = $('.flipbook').offset(),
+		boundH = height - flipbookOffset.top - $('.flipbook').height(),
+		marginTop = (boundH - $('.thumbnails > div').height()) / 2;
+
+	if (marginTop<0) {
+		$('.thumbnails').css({height:1});
+	} else {
+		$('.thumbnails').css({height: boundH});
+		$('.thumbnails > div').css({marginTop: marginTop});
+	}
+
+	if (flipbookOffset.top<$('.made').height())
+		$('.made').hide();
+	else
+		$('.made').show();
+
+	$('.flipbook').addClass('animated');
+	
+}
+
+function disableControls(page) {
+		if (page==1)
+			$('.previous-button').hide();
+		else
+			$('.previous-button').show();
+					
+		if (page==$('.flipbook').turn('pages'))
+			$('.next-button').hide();
+		else
+			$('.next-button').show();
+}
+
+// Calculate the width and height of a square within another square
+
+function calculateBound(d) {
+	
+	var bound = {width: d.width, height: d.height};
+
+	if (bound.width>d.boundWidth || bound.height>d.boundHeight) {
+		
+		var rel = bound.width/bound.height;
+
+		if (d.boundWidth/rel>d.boundHeight && d.boundHeight*rel<=d.boundWidth) {
+			
+			bound.width = Math.round(d.boundHeight*rel);
+			bound.height = d.boundHeight;
+
+		} else {
+			
+			bound.width = d.boundWidth;
+			bound.height = Math.round(d.boundWidth/rel);
+		
+		}
+	}
+		
+	return bound;
+}
